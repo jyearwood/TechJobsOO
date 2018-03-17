d2f5228e0b21e75999112ca680382a83760e03b2
Janelle Yearwood <msyearwood@yahoo.com>
2018-03-17 15:11:54 -0700

    initial commit

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
 using TechJobs.ViewModels;
+using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
@@ -18,9 +19,12 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            // TODO #1 - get the Job with the given ID and pass it into the view
+			Job data = jobData.Find(id);
 
-            return View();
+			// TODO #1 - get the Job with the given ID and pass it into the view
+			
+
+            return View(data);
         }
 
         public IActionResult New()
@@ -32,9 +36,24 @@ namespace TechJobs.Controllers
         [HttpPost]
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
-            // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
+			// TODO #6 - Validate the ViewModel and if valid, create a 
+			// new Job and add it to the JobData data store. Then
+			// redirect to the Job detail (Index) action/view for the new Job.
+
+			if (ModelState.IsValid)
+			{
+				Job newJob = new Job
+				{
+					Name = newJobViewModel.Name,
+					Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+					Location = jobData.Locations.Find(newJobViewModel.LocationID),
+					CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+					PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+				};
+
+				jobData.Jobs.Add(newJob);
+				return Redirect(string.Format("/Job?id={0}", newJob.ID));
+			}
 
             return View(newJobViewModel);
         }
---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+		//Colums for display
+		public List<JobFieldType> Columns { get; set; }
+
+		//View title
+		public string Title { get; set; } = "";
+
+
+		public BaseViewModel()
+		{
+
+			//populate the list of all the columns
+
+			Columns = new List<JobFieldType>();
+
+			foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+			{
+				Columns.Add(enumVal);
+			}
+		}
+    }
+}
